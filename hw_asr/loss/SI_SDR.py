e03a9822bcecf7d98de3be66ab1d37c17d835380
Oleg <oleg.kurilov@raiffeisen.ru>
2023-11-10 23:48:02 +0300

    fix si_sdr

--- hw_asr/loss/SI_SDR.py
+++ hw_asr/loss/SI_SDR.py
@@ -1,4 +1,3 @@
-import numpy as np
 import torch.nn as nn
 import torch
 
@@ -38,6 +37,8 @@ class FinalLoss(nn.Module):
 
     def forward(self, short_pred, middle_pred, long_pred, target, log_probs, speaker_id):
         sisdr = self.sisdr(short_pred, middle_pred, long_pred, target)
-        speaker_id_tensor = torch.Tensor(speaker_id).type(torch.LongTensor).to("cuda")
-        return sisdr + self.gamma * self.celoss(log_probs, speaker_id_tensor.detach())
+        ce_loss = self.celoss(log_probs, torch.Tensor(speaker_id).type(torch.LongTensor).to("cuda")).detach()
+        loss = sisdr + self.gamma * ce_loss
+        return loss
+
 
