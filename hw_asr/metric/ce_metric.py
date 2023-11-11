93f3b5169ae631ab7e1f0b30736db5b25fe6f121
Oleg <oleg.kurilov@raiffeisen.ru>
2023-11-11 23:20:12 +0300

    fix ce metric

--- hw_asr/metric/ce_metric.py
+++ hw_asr/metric/ce_metric.py
@@ -8,6 +8,6 @@ class CEMetric(BaseMetric):
         super().__init__(*args, **kwargs)
 
     def __call__(self, **batch):
-        log_probs = batch["log_probs"].to("cpu").detach()
+        log_probs = batch["logits"].to("cpu").detach()
         speaker_id = batch["speaker_id"].to("cpu").detach()
         return self.ce(log_probs, speaker_id).item()
