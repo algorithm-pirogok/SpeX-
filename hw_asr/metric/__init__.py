8ae0ea35d247cccfb38a969a424d62182d99fa41
Oleg <oleg.kurilov@raiffeisen.ru>
2023-11-11 18:53:06 +0300

    ce metric

--- hw_asr/metric/__init__.py
+++ hw_asr/metric/__init__.py
@@ -1,7 +1,10 @@
 from hw_asr.metric.pesq_metric import PESQMetric
 from hw_asr.metric.sisdr_metric import SISDRMetric
+from hw_asr.metric.ce_metric import CEMetric
+
 
 __all__ = [
     "SISDRMetric",
-    "PESQMetric"
+    "PESQMetric",
+    "CEMetric"
 ]
--- hw_asr/metric/ce_metric.py
+++ hw_asr/metric/ce_metric.py
@@ -2,7 +2,7 @@ import torch.nn as nn
 from hw_asr.base.base_metric import BaseMetric
 
 
-class SISDRMetric(BaseMetric):
+class CEMetric(BaseMetric):
     def __init__(self, *args, **kwargs):
         self.ce = nn.CrossEntropyLoss
         super().__init__(*args, **kwargs)
