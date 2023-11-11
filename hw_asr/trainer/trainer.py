3f23b468714ae1810c9fafb41694f1a4dd6b212b
Oleg <oleg.kurilov@raiffeisen.ru>
2023-11-11 17:33:53 +0300

    config

--- hw_asr/trainer/trainer.py
+++ hw_asr/trainer/trainer.py
@@ -108,6 +108,7 @@ class Trainer(BaseTrainer):
                     continue
                 else:
                     raise e
+            print("AFTER PROCESS_BATCH")
             self.train_metrics.update("grad norm", self.get_grad_norm())
             if batch_idx % self.log_step == 0:
                 self.writer.set_step((epoch - 1) * self.len_epoch + batch_idx)
