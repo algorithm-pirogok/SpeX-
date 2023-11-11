ab6e812c773def1402258a6fcda09068bb97c66e
Oleg <oleg.kurilov@raiffeisen.ru>
2023-11-11 19:08:58 +0300

    fix reteaching

--- hw_asr/metric/ce_metric.py
+++ hw_asr/metric/ce_metric.py
@@ -10,5 +10,4 @@ class CEMetric(BaseMetric):
     def __call__(self, **batch):
         log_probs = batch["log_probs"].to("cpu").detach()
         speaker_id = batch["speaker_id"].to("cpu").detach()
-        print("CE LOSS", log_probs, speaker_id)
         return self.ce(log_probs, speaker_id).item()
--- hw_asr/metric/sisdr_metric.py
+++ hw_asr/metric/sisdr_metric.py
@@ -13,7 +13,4 @@ class SISDRMetric(BaseMetric):
         middle = batch["middle"].to("cpu").detach()
         target = batch["target"].to("cpu").detach()
         ref = batch["ref"].to("cpu").detach()
-        print("SHORT/MIDDLE SI_SDR:", self.si_sdr(short, middle))
-        print("SHORT/target SI_SDR:", self.si_sdr(short, target))
-        print("SHORT/ref SI_SDR:", self.si_sdr(short, ref))
         return self.si_sdr(short, target)
