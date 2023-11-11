c477df033ca2e8484890c41feddbd9c928b9929e
Oleg <oleg.kurilov@raiffeisen.ru>
2023-11-11 22:00:48 +0300

    change loss

--- hw_asr/loss/SI_SDR.py
+++ hw_asr/loss/SI_SDR.py
@@ -1,5 +1,6 @@
 import torch.nn as nn
 import torch
+from torchmetrics.audio import ScaleInvariantSignalDistortionRatio
 
 
 class SI_SDR(nn.Module):
@@ -13,22 +14,24 @@ class SI_SDR(nn.Module):
         self.middle_coeff = middle
         self.long_coeff = 1 - short - middle
         self.eps = eps
+        self.si_sdr = ScaleInvariantSignalDistortionRatio(zero_mean=False)
 
     def _compute_sisdr(self, estimate, target):
         if estimate.shape != target.shape:
             raise ValueError("Inputs must have the same shape")
 
         # Computing the numerator and denominator components of the SI-SDR
-        product = torch.sum(target * estimate, dim=-1, keepdim=True) * target / torch.sum(target * target, dim=-1,
+        product = torch.sum(target * estimate, dim=-1, keepdim=True) * target / torch.sum(target ** 2, dim=-1,
                                                                                           keepdim=True)
         error = estimate - product
 
         # Computing SI-SDR
-        SISDR = 10 * torch.log10(torch.sum(product * product, dim=-1) / torch.sum(error * error, dim=-1))
+        SISDR = 10 * torch.log10(torch.sum(product ** 2, dim=-1) / torch.sum(error ** 2, dim=-1))
 
         # Averaging SI-SDR over the batch
         loss = -torch.mean(SISDR)
-
+        print("PRED_LOSS:", loss)
+        product("REAL LOSS:", self.si_sdr(estimate, target))
         return loss
 
     def forward(self, short_pred, middle_pred, long_pred, target):
