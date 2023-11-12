5ae357ed152d9822193dec3b78c3e2be3ed45d5a
Oleg <oleg.kurilov@raiffeisen.ru>
2023-11-12 11:50:27 +0300

    add comb metric

--- hw_asr/metric/sisdr_metric.py
+++ hw_asr/metric/sisdr_metric.py
@@ -4,8 +4,9 @@ from hw_asr.base.base_metric import BaseMetric
 
 
 class SISDRMetric(BaseMetric):
-    def __init__(self, zero_mean: bool, *args, **kwargs):
+    def __init__(self, zero_mean: bool, comb: bool, *args, **kwargs):
         self.si_sdr = ScaleInvariantSignalDistortionRatio(zero_mean=zero_mean)
+        self.comb = comb
         super().__init__(*args, **kwargs)
 
     def __call__(self, **batch):
@@ -13,5 +14,5 @@ class SISDRMetric(BaseMetric):
         middle = batch["middle"].to("cpu").detach()
         long = batch["long"].to("cpu").detach()
         target = batch["target"].to("cpu").detach()
-        pred = short * 0.8 + middle * 0.1 + long * 0.1
+        pred = short * 0.8 + middle * 0.1 + long * 0.1 if self.comb else short
         return self.si_sdr(pred, target)
